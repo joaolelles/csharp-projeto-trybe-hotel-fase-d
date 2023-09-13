0cf439b117604cf0cddbde7efc9277fda218fa81
João <joaolelles7@gmail.com>
2023-09-13 16:13:29 -0300

    Fix: UserRepository

--- src/TrybeHotel/Repository/UserRepository.cs
+++ src/TrybeHotel/Repository/UserRepository.cs
@@ -1,5 +1,6 @@
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
+using System.ComponentModel.DataAnnotations;
 
 namespace TrybeHotel.Repository
 {
@@ -17,23 +18,52 @@ namespace TrybeHotel.Repository
 
         public UserDto Login(LoginDto login)
         {
-            throw new NotImplementedException();
+            User? userByLogin = _context.Users.FirstOrDefault(user => user.Email == login.Email
+            && user.Password == login.Password);
+
+            if (userByLogin == null)
+            {
+                return null!;
+            }
+            return new UserDto
+            {
+                UserId = userByLogin!.UserId,
+                Name = userByLogin.Name,
+                Email = userByLogin.Email,
+                UserType = userByLogin.UserType,
+            };
         }
         public UserDto Add(UserDtoInsert user)
         {
-            throw new NotImplementedException();
+            var newUser = new User()
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                UserType = "client",
+            };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+            var postUser = new UserDto
+            {
+                UserId = newUser.UserId,
+                Name = newUser.Name,
+                Email = newUser.Email,
+                UserType = newUser.UserType,
+            };
+            return postUser;
         }
 
         public UserDto GetUserByEmail(string userEmail)
         {
             UserDto? userByEmail = _context.Users.Where(user => user.Email == userEmail)
-           .Select(user => new UserDto
-           {
-               UserId = user.UserId,
-               Name = user.Name,
-               Email = user.Email,
-               UserType = user.UserType,
-           }).FirstOrDefault();
+            .Select(user => new UserDto
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                Email = user.Email,
+                UserType = user.UserType,
+            }).FirstOrDefault();
 
             if (userByEmail == null)
             {
@@ -48,97 +78,10 @@ namespace TrybeHotel.Repository
             {
                 UserId = user.UserId,
                 Name = user.Name,
-                Email = user.Email,using TrybeHotel.Models;
-            using TrybeHotel.Dto;
-            using System.ComponentModel.DataAnnotations;
-
-namespace TrybeHotel.Repository
-    {
-        public class UserRepository : IUserRepository
-        {
-            protected readonly ITrybeHotelContext _context;
-            public UserRepository(ITrybeHotelContext context)
-            {
-                _context = context;
-            }
-            public UserDto GetUserById(int userId)
-            {
-                throw new NotImplementedException();
-            }
-
-            public UserDto Login(LoginDto login)
-            {
-                User? userByLogin = _context.Users.FirstOrDefault(user => user.Email == login.Email
-                && user.Password == login.Password);
-
-                if (userByLogin == null)
-                {
-                    return null!;
-                }
-                return new UserDto
-                {
-                    UserId = userByLogin!.UserId,
-                    Name = userByLogin.Name,
-                    Email = userByLogin.Email,
-                    UserType = userByLogin.UserType,
-                };
-            }
-            public UserDto Add(UserDtoInsert user)
-            {
-                var newUser = new User()
-                {
-                    Name = user.Name,
-                    Email = user.Email,
-                    Password = user.Password,
-                    UserType = "client",
-                };
-                _context.Users.Add(newUser);
-                _context.SaveChanges();
-                var postUser = new UserDto
-                {
-                    UserId = newUser.UserId,
-                    Name = newUser.Name,
-                    Email = newUser.Email,
-                    UserType = newUser.UserType,
-                };
-                return postUser;
-            }
-
-            public UserDto GetUserByEmail(string userEmail)
-            {
-                UserDto? userByEmail = _context.Users.Where(user => user.Email == userEmail)
-                .Select(user => new UserDto
-                {
-                    UserId = user.UserId,
-                    Name = user.Name,
-                    Email = user.Email,
-                    UserType = user.UserType,
-                }).FirstOrDefault();
-
-                if (userByEmail == null)
-                {
-                    return null!;
-                }
-                return userByEmail;
-            }
-
-            public IEnumerable<UserDto> GetUsers()
-            {
-                var user = _context.Users.Select(user => new UserDto
-                {
-                    UserId = user.UserId,
-                    Name = user.Name,
-                    Email = user.Email,
-                    UserType = user.UserType,
-                });
-                return user;
-            }
-
-        }
-    }
-    UserType = user.UserType,
+                Email = user.Email,
+                UserType = user.UserType,
             });
-return user;
+            return user;
         }
 
     }
